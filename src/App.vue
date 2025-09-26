1a6f989e1491f83ce59542bda3bf737a8b2cdc3c
willy <willybalt.2017@gmail.com>
2025-09-25 21:43:14 -0400

    creacion del proyecto Vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+let titulo = ref<string>("proyecto vue con ts");
+let numero = ref<number>(200);
+
+
+</script>
+
+<template>
+  <h1>{{ titulo }}</h1>
+  <p>{{ numero }}</p>
+</template>
